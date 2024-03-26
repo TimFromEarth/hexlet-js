11daf6519d552222cc9d8d18d90ab10922d3e082
Temirlan Kozhagulov <kozhagulovt@gmail.com>
2024-03-26 23:40:34 +0500

    modified index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import _ from "lodash";
+import someFunction from './src/index.js';
 
 console.log('Hello, Hexlet!');
 console.log(_.last(['one', 'two']));
+
+const num = someFunction(2);
+
+console.log(`Цифра: ${num}`);
